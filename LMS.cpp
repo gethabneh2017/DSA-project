7c5553a0f4d01816df96a9f711a59c0c8b60659d
bana <bandis1921@gmail.com>
2025-04-01 00:24:45 -0700

    Update LMS.cpp

--- LMS.cpp
+++ LMS.cpp
@@ -1,4 +1,40 @@
+//this is for banderaws
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
+// Book structure for array storage
+struct Book {
+    int id;
+    string title;
+    string author;
+    string genre;
+    bool isIssued = false;
+
+    void display() const {
+        cout << "ID: " << id << ", Title: " << title << ", Author: " << author
+             << ", Genre: " << genre << ", Issued: " << (isIssued ? "Yes" : "No") << endl;
+    }
+};
+//bellow this is basha code
+//basha contribute 
+// Library Member as a linked list node
+class Member {
+public:
+    int id;
+    string name;
+    int borrowedBooks = 0;
+    bool isActive = true;
+    Member* next = nullptr;
+
+    Member(int id, const string& name) : id(id), name(name) {}
+
+    void display() const {
+        cout << "ID: " << id << ", Name: " << name
+             << ", Borrowed: " << borrowedBooks << ", Status: " << (isActive ? "Active" : "Inactive") << endl;
+    }
+};
 //this is bashas
 class Member {
 public:
