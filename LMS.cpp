9a66fbae0e6c93c3375279e82148e4fc5386d2fc
fafmik <fafmik03@gmail.com>
2025-04-01 10:49:11 +0300

    Update LMS.cpp
    This the option  displayed in console for the sake select what we want to do

--- LMS.cpp
+++ LMS.cpp
@@ -51,3 +51,21 @@ public:
              << ", Borrowed: " << borrowedBooks << ", Status: " << (isActive ? "Active" : "Inactive") << endl;
     }
 };
+//this is from mikyas fikre
+// Function to display the menu
+void displayMenu() {
+    cout << "\nLibrary Management System Menu:\n";
+    cout << "1. Add Book\n";
+    cout << "2. Search Book\n";
+    cout << "3. Update Book\n";
+    cout << "4. Delete Book\n";
+    cout << "5. Add Member\n";
+    cout << "6. Search Member\n";
+    cout << "7. Update Member Status\n";
+    cout << "8. Delete Member\n";
+    cout << "9. Issue Book\n";
+    cout << "10. Return Book\n";
+    cout << "11. Save Books to File\n";
+    cout << "0. Exit\n";
+    cout << "Choose an option: ";
+}
